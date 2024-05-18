7b10f30665e07363aaf2f917fdd991cf3fab5000
sherazanwar21 <sheraz786868@gmail.com>
2024-05-18 19:02:33 +0000

    Destroy me

--- script6/output.tf
+++ script6/output.tf
@@ -1,15 +1,15 @@
-output "ec2pubid" {
-  value = aws_instance.myinstance.public_ip
-}
+# output "ec2pubid" {
+#   value = aws_instance.myinstance.public_ip
+# }
 
-output "ec2id" {
-  value = aws_instance.myinstance.id
-}
+# output "ec2id" {
+#   value = aws_instance.myinstance.id
+# }
 
-output "ec2pubdns" {
-  value = aws_instance.myinstance.public_dns
-}
+# output "ec2pubdns" {
+#   value = aws_instance.myinstance.public_dns
+# }
 
-output "ec2tags" {
-  value = aws_instance.myinstance.tags
-}
+# output "ec2tags" {
+#   value = aws_instance.myinstance.tags
+# }
